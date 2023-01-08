8e838feda121738921eaf4b80ea36e3efb68d1d7
Raspberry Pi <raspberrypisig@gmail.com>
2023-01-09 09:32:47 +1100

    filling in main

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-from abc import ABC, abstractmethod
-from typing import List, Self
-from enum import Enum
+
+from collections import defaultdict
 import schemdraw
+from visitor import SchemDrawVisitor
+from circuitjsgrammar import CircuitJSGrammar
+from point import Point
 
 schemdraw.use('svg')
 schemdraw.svgconfig.text = 'path'
 schemdraw.svgconfig.svg2 = False
 schemdraw.svgconfig.precision = 2
 
+class DrawingState:
+    def __init__(self) -> None:
+        self._lookup = defaultdict(list)
+        self._elements_to_draw = []
+        self._elements_drawn = []
+        self._candidate_coords = []
+        self._drawn_list = {}
+        self._number_of_elements = 0
+
 def main(input_file: str, output_file: str) -> None:
-    pass
+    visitor = SchemDrawVisitor()
+    grammar = CircuitJSGrammar()
+    drawing_state = 
+    
+    with open(input_file, "r") as f:
+        f.readline()
+        for line in f:            
+            parsing_result = grammar.parse(line)
+            #print(parsing_result.is_valid)
+            if parsing_result.is_valid:
+                pass
 
 if __name__ == "__main__":
     main("tests/test001.txt", "out/test001.svg")   
--- visitor.py
+++ visitor.py
@@ -9,8 +9,21 @@ class Visitor(ABC):
     def visit_any(self, d: Drawing, element: ElectronicComponent) -> None:
         pass
 
-    @abstractmethod
-    def visit_three_terminal(self, d: Drawing, element: ElectronicComponent) -> None:
-        pass
+    #@abstractmethod
+    #def visit_three_terminal(self, d: Drawing, element: ElectronicComponent) -> None:
+    #    pass
+
+class SchemDrawVisitor(Visitor):    
+    def visit_any(self, d, element):
+        d.push()
+        element_args = {}
+        element_args['d'] = element.direction
+        if element.shouldReverse:
+            element_args["reverse"] = True
+        c = element.getElement()(**element_args)
+        d += c
+        here = d.here
+        d.pop()
+        return here
 
 
