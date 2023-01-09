aefbba0f6a6f1ee6dd54b75f73f3928d7088c987
Raspberry Pi <raspberrypisig@gmail.com>
2023-01-09 18:07:52 +1100

    major refactoring and reordering of code

---
+++ src/component_warehouse.py
@@ -0,0 +1,22 @@
+
+
+
+
+from src.electronic_component import ElectronicComponent
+
+
+class ComponentWarehouse:
+    def __init__(self) -> None:
+        self.classes: dict[str, ElectronicComponent] = {}
+
+    def add_class(self, c: ElectronicComponent) -> None:
+        self.classes[c.name] = c
+
+    def component(self, c: ElectronicComponent) -> ElectronicComponent:
+        self.add_class(c)
+        return c 
+
+    def __getitem__(self, n: str) -> ElectronicComponent:
+        return self.classes[n]
+
+component_warehouse = ComponentWarehouse()
