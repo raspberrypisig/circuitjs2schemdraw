5043a87a26fd771a801af5df96d54fc80d30403c
Raspberry Pi <raspberrypisig@gmail.com>
2023-01-11 10:26:11 +1100

    now need to create lookup

--- src/circuitjs_to_schemdraw.py
+++ src/circuitjs_to_schemdraw.py
@@ -1,7 +1,7 @@
 
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import List, Optional
 from .drawing_state import DrawingState
 from .circuitjsgrammar import CircuitJSGrammar
 from .visitor import SchemDrawVisitor
@@ -17,17 +17,26 @@ class ComponentManifest:
 def create_component_manifest(component_name:str, start_coords: Point, end_coords: Point, value: Optional[float] = None) -> ComponentManifest:    
     return ComponentManifest(component_name, start_coords, end_coords, value)
 
+def create_lookup(component_manifests: List[ComponentManifest]):
+    pass
+
 def circuitjs_to_schemdraw(input_file: str, output_file: str) -> None:
     visitor = SchemDrawVisitor()
     grammar = CircuitJSGrammar()
     drawing_state = DrawingState()
-    
+    component_manifests = []
+    lookup = {}
+
     with open(input_file, "r") as f:
         f.readline()
         for line in f:            
             parsing_result = grammar.parse(line)
-            if parsing_result.is_valid:
-                
+            if parsing_result.is_valid:                
                 component_name, start_terminal, end_terminal, value_literal = grammar.extract(parsing_result)        
-                component = create_component_manifest(component_name, start_coords=Point(*start_terminal), end_coords=Point(*end_terminal), value=value_literal)
-                print(component)
+                component_manifest = create_component_manifest(component_name, start_coords=Point(*start_terminal), end_coords=Point(*end_terminal), value=value_literal)
+                component_manifests.append(component_manifest)
+    
+    print(component_manifests)
+    lookup = create_lookup(component_manifests)
+
+    
