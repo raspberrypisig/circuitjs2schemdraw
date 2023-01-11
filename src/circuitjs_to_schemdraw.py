2ae04f09fc2ee5a9b71cfd8fccba35fcee47eb8f
Raspberry Pi <raspberrypisig@gmail.com>
2023-01-11 22:32:10 +1100

    needs some clearer thinking

--- src/circuitjs_to_schemdraw.py
+++ src/circuitjs_to_schemdraw.py
@@ -24,13 +24,17 @@ class CircuitJSToSchemDraw:
     def create_component_manifest(self, component_name:str, start_coords: Point, end_coords: Point, value: Optional[float] = None) -> ComponentManifest:    
         return ComponentManifest(component_name, start_coords, end_coords, value)
 
-    def create_lookup(self):
+    def circuitjs_element_class(self, component_manifest):
         component_repo = component_repository()
+        element_class = component_repo[component_manifest.component_name]
+        return element_class
+
+    def create_lookup(self):
         lookup = defaultdict(list)
 
         for component_manifest in self.component_manifests:
             #print(component_repo[component_manifest.component_name])
-            element_class = component_repo[component_manifest.component_name]
+            element_class = self.circuitjs_element_class(component_manifest)
             anchors = element_class.anchors(component_manifest.start_coords, component_manifest.end_coords)
             for anchor, terminal in anchors:
                 lookup[terminal].append(  (component_manifest, anchor) )
@@ -39,13 +43,20 @@ class CircuitJSToSchemDraw:
 
 
     def find_first(self, points):
-        result = points.pop()
-        for point in points:
-            if point < result:
-                result = point
-        return result
+        return sorted(points).pop()
+        #result = points.pop()
+        #for point in points:
+        #    if point < result:
+        #        result = point
+        #return result
 
-    def convert(self) -> None:
+    def other_anchors(self, component_manifest: ComponentManifest, excluded_anchor):
+        element_class = self.circuitjs_element_class(component_manifest)
+        anchors = element_class.anchors(component_manifest.start_coords, component_manifest.end_coords)        
+        anchors = [(anchor,terminal) for anchor,terminal in anchors if not anchor == excluded_anchor]            
+        return anchors
+
+    def parse_input_file(self):
         with open(self.input_file, "r") as f:
             f.readline()
             for line in f:            
@@ -54,32 +65,37 @@ class CircuitJSToSchemDraw:
                     component_name, start_terminal, end_terminal, value_literal = self.grammar.extract(parsing_result)        
                     component_manifest = self.create_component_manifest(component_name, start_coords=Point(*start_terminal), end_coords=Point(*end_terminal), value=value_literal)
                     self.component_manifests.append(component_manifest)
-        
+
+    def convert(self) -> None:
+        self.parse_input_file()
         #print(component_manifests)
         lookup = self.create_lookup()
         #print(lookup)
         lookup_terminal = self.find_first(list(lookup.keys()))
-        #print(first_terminal)
+        print(lookup_terminal)
         drawing_order = []
 
         drawn_anchors = []
-        candidate_anchors = set([])
+        candidate_anchors = []
 
-        print(f"number of items in components manifest: {len(self.component_manifests)}")
-        for i in range(1):
+        #print(f"number of items in components manifest: {len(self.component_manifests)}")
+        for i in range(3):
             terminals = lookup[lookup_terminal]
+            if lookup_terminal in candidate_anchors:
+                candidate_anchors.remove(lookup_terminal)
             for terminal in terminals:
                 drawing_order.append(terminal)
                 component_manifest, anchor = terminal
-                print(component_manifest)
-                print(anchor)
-                self.component_manifests.remove(component_manifest)
-                print(f"number of items in components manifest: {len(self.component_manifests)}")
-                #anchors = get_anchors(component_manifest)
-                # for other_anchor in anchors:
-                #     drawn_anchors.append(lookup_terminal)
-                #     candidate_anchors.append(anchors[other])
+                #self.component_manifests.remove(component_manifest)
+                anchors = self.other_anchors(component_manifest, anchor)
+                for _, other_terminal in anchors:
+                    drawn_anchors.append(lookup_terminal)
+                    candidate_anchors.append(other_terminal)
                 #     first_terminal = find_first(drawn_anchors)
 
-        print(drawing_order)
+        #print(drawing_order)
+        print(candidate_anchors)
+        lookup_terminal = self.find_first(candidate_anchors)
+        
+
 
---
+++ src/schemdraw_manifest.py
@@ -0,0 +1,7 @@
+
+class SchemdrawElementManifest:
+    def __init__(self, element_class, constructor_args) -> None:
+        self.element_class = element_class
+        self.constructor_args = constructor_args
+
+ 
